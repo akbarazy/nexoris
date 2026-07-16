e9671e43f83cbc2518fbf83ae9624af63d5fe8d8
akbarazy <muh.akbar.alfarizy@gmail.com>
2026-07-16 21:52:44 +0700

    Add screen basic functionality

---
+++ include/core/screen.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <raylib.h>
+
+class Screen {
+public:
+    static constexpr int CANVAS_WIDTH = 1280;
+    static constexpr int CANVAS_HEIGHT = 720;
+    RenderTexture2D canvas;
+
+    Screen();
+    void Render();
+};
---
+++ src/core/screen.cpp
@@ -0,0 +1,36 @@
+#include <raylib.h>
+#include <algorithm>
+#include "core/screen.hpp"
+
+Screen::Screen() {
+    SetConfigFlags(FLAG_WINDOW_UNDECORATED);
+    InitWindow(CANVAS_WIDTH, CANVAS_HEIGHT, "Nexoris");
+    
+    int monitor = GetCurrentMonitor();
+    SetWindowSize(GetMonitorWidth(monitor), GetMonitorHeight(monitor));
+    SetWindowPosition(0, 0);
+
+    SetTargetFPS(60);
+
+    canvas = LoadRenderTexture(CANVAS_WIDTH, CANVAS_HEIGHT);
+    SetTextureFilter(canvas.texture, TEXTURE_FILTER_POINT);
+}
+
+void Screen::Render() {
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    float scale = std::min((float)GetScreenWidth() / CANVAS_WIDTH, (float)GetScreenHeight() / CANVAS_HEIGHT);
+    Rectangle source = { 0.0f, 0.0f, (float)canvas.texture.width, (float)-canvas.texture.height };
+
+    Rectangle dest = { 
+        (GetScreenWidth() - ((float)CANVAS_WIDTH * scale)) * 0.5f,
+        (GetScreenHeight() - ((float)CANVAS_HEIGHT * scale)) * 0.5f,
+        (float)CANVAS_WIDTH * scale, 
+        (float)CANVAS_HEIGHT * scale 
+    };
+
+    DrawTexturePro(canvas.texture, source, dest, { 0.0f, 0.0f }, 0.0f, WHITE);
+
+    EndDrawing();
+}
