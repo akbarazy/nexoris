e6fec431ef22ceb72b71fde79c0c1295530506d7
akbarazy <muh.akbar.alfarizy@gmail.com>
2026-07-17 21:34:09 +0700

    Refactor main to initialize game

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,8 @@
-#include <raylib.h>
+#include "core/game.hpp"
 
 int main()
 {
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
-    InitWindow(screenWidth, screenHeight, "Nexoris");
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(BLACK);
-        
-        DrawText("Hello World", 500, 300, 50, WHITE);
-        
-        EndDrawing();
-    }
-
-    CloseWindow();
+    Game game;
+    game.Run();
     return 0;
 }
